0481108ee2f77ea6b68af5939691b96b5ed72e55
AHurowitz <thicclatka@users.noreply.github.com>
2026-04-28 02:08:39 -0400

    fix(homebrew): add hdf5 dependency for netcdf build

--- Formula/ublx.rb
+++ Formula/ublx.rb
@@ -10,11 +10,21 @@ class Ublx < Formula
   depends_on "rust" => :build
 
   depends_on "ffmpeg"
+  depends_on "hdf5"
   depends_on "netcdf"
   depends_on "poppler"
   depends_on "tree"
 
   def install
+    hdf5 = Formula["hdf5"].opt_prefix
+    netcdf = Formula["netcdf"].opt_prefix
+    ENV["HDF5_DIR"] = hdf5
+    ENV["HDF5_ROOT"] = hdf5
+    ENV["HDF5_INCLUDE_DIR"] = "#{hdf5}/include"
+    ENV["HDF5_LIB_DIR"] = "#{hdf5}/lib"
+    ENV["NETCDF_DIR"] = netcdf
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{hdf5}/lib/pkgconfig"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{netcdf}/lib/pkgconfig"
     system "cargo", "install", *std_cargo_args
   end
 
